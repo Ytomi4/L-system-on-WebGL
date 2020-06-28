05efbd5d2b3a49ab1c9b368320763baf7e21d803
Ytomi4 <kotirashuhei@gmail.com>
2020-06-28 18:49:41 +0900

    bug fix

--- windowsResize.js
+++ windowsResize.js
@@ -1,25 +1,16 @@
-//reference : https://qiita.com/Katumadeyaruhiko/items/9c0a82865c239b95a809
-
 var container;
 var canvas;
 var timer = 0;
 
 function init(){
-    canvas = document.getElementById('canvas');
-    container = document.createElement('div');
-    container.style.width = window.innerWidth + 'px';
-    container.style.height = window.innerHeight + 'px';
-    container.style.overflow = 'hidden';
-    container.appendChild(canvas);
-    document.body.appendChild(container);
-    document.body.style.margin = '0px';
+    container = document.getElementById('unityContainer');
+    document.body.style.margin = "0px";
 }
 
 function resize(){
-    container.style.width = window.innerWidth + 'px';
-    container.style.height = window.innerHeight + 'px';
-    coanvas.width = window.innerWidth * window.devicePixelRatio;
-    canvas.height = window.innerHeight * window.devicePixelRatio;
+    container.style.width = window.innerWidth + "px";
+    container.style.height = window.innerHeight + "px";
+    container.style.margin = "0px";
 }
 
 window.onload = function(){
